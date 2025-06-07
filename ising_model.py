5f6e0b13b1cbbeb9b22862ecda0cbdcf69bc94cf
unrealda <164433513+unrealda@users.noreply.github.com>
2025-06-07 15:38:52 +0800

    Update ising_model.py

--- ising_model.py
+++ ising_model.py
@@ -1,61 +1,91 @@
+# ising_model.py
 import numpy as np
+import pandas as pd
+from collections import deque
 
-def run_ising_simulation(L, Tmin, Tmax, nT, Ntrial, folder):
+def square_neighbors(L):
+    neighbors = {}
+    for i in range(L):
+        for j in range(L):
+            site = i * L + j
+            neighbors[site] = []
+            for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]:
+                ni, nj = (i+dx)%L, (j+dy)%L
+                neighbor_site = ni * L + nj
+                neighbors[site].append(neighbor_site)
+    return neighbors
+
+def triangular_neighbors(L):
+    neighbors = {}
+    for i in range(L):
+        for j in range(L):
+            site = i * L + j
+            neighbors[site] = []
+            for dx, dy in [(-1,0),(1,0),(0,-1),(0,1),(-1,1),(1,-1)]:
+                ni, nj = (i+dx)%L, (j+dy)%L
+                neighbor_site = ni * L + nj
+                neighbors[site].append(neighbor_site)
+    return neighbors
+
+def wolff_algorithm(L, neighbors, T, Ntrial):
     N = L * L
-    T_list = np.linspace(Tmin, Tmax, nT)
+    beta = 1.0 / T
+    p = 1 - np.exp(-2 * beta)
+    S = np.random.choice([-1, 1], size=N)
     M_list = []
-    Chi_list = []
-    spin_snapshots = []
+    cluster_sizes = []
+
+    for _ in range(Ntrial):
+        seed = np.random.randint(N)
+        cluster = set([seed])
+        pocket = deque([seed])
+
+        while pocket:
+            site = pocket.pop()
+            for neigh in neighbors[site]:
+                if S[neigh] == S[site] and neigh not in cluster and np.random.rand() < p:
+                    cluster.add(neigh)
+                    pocket.append(neigh)
+        S[list(cluster)] *= -1
+        M = np.sum(S) / N
+        M_list.append(M)
+        cluster_sizes.append(len(cluster))
+
+    return M_list, cluster_sizes
+
+def simulate(L, lattice, Tmin, Tmax, nT, Ntrial):
+    if lattice.lower() == 'square':
+        neighbors = square_neighbors(L)
+    elif lattice.lower() == 'triangular':
+        neighbors = triangular_neighbors(L)
+    else:
+        raise ValueError("Unknown lattice type")
+
+    T_list = np.linspace(Tmin, Tmax, nT)
+    M_mean = []
+    M_var = []
+    Chi = []
+    Binder = []
+    all_cluster_sizes = []
 
     for T in T_list:
-        beta = 1.0 / T
-        p = 1 - np.exp(-2 * beta)
-        S = np.random.choice([-1, 1], size=N)  # initial spin state
+        M_list, cluster_sizes = wolff_algorithm(L, neighbors, T, Ntrial)
+        M_arr = np.array(M_list)
+        M_abs = np.abs(M_arr)
+        M_mean.append(np.mean(M_abs))
+        M_var.append(np.var(M_arr))
+        Chi.append(L*L * (np.mean(M_arr**2) - np.mean(M_abs)**2) / T)
+        M2 = np.mean(M_arr**2)
+        M4 = np.mean(M_arr**4)
+        Binder.append(1 - M4 / (3 * M2**2))
+        all_cluster_sizes.extend(cluster_sizes)
 
-        magnetizations = []
+    result_df = pd.DataFrame({
+        "Temperature": T_list,
+        "Magnetization": M_mean,
+        "Magnetization_Var": M_var,
+        "Susceptibility": Chi,
+        "Binder_Ratio": Binder
+    })
 
-        for _ in range(Ntrial):
-            k = np.random.randint(N)
-            cluster = [k]
-            pocket = [k]
-
-            while pocket:
-                s = pocket.pop(np.random.randint(len(pocket)))
-                neighbors = get_neighbors(s, L)
-                for n in neighbors:
-                    if S[n] == S[s] and n not in cluster:
-                        if np.random.rand() < p:
-                            cluster.append(n)
-                            pocket.append(n)
-
-            for site in cluster:
-                S[site] *= -1
-
-            magnetizations.append(np.sum(S))
-
-        M_avg = np.mean(np.abs(magnetizations)) / N
-        M2_avg = np.mean(np.array(magnetizations) ** 2) / (N ** 2)
-        Chi = (N / T) * (M2_avg - M_avg ** 2)
-
-        M_list.append(M_avg)
-        Chi_list.append(Chi)
-
-        # Store snapshot in 2D
-        S2D = S.reshape((L, L))
-        spin_snapshots.append(S2D)
-
-        # Save raw snapshot
-        np.savetxt(f"{folder}/snapshot_T{T:.3f}.dat", S2D, fmt='%d')
-
-    return T_list, spin_snapshots, M_list, Chi_list
-
-def get_neighbors(index, L):
-    row = index // L
-    col = index % L
-    neighbors = [
-        ((row - 1) % L) * L + col,  # up
-        ((row + 1) % L) * L + col,  # down
-        row * L + (col - 1) % L,    # left
-        row * L + (col + 1) % L     # right
-    ]
-    return neighbors
+    return result_df, all_cluster_sizes
